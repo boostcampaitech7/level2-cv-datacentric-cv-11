d06e2312ced8f218970ca063ef2033e17f6d28d9
Daehwan <aiart3536@gmail.com>
2024-11-01 00:28:58 +0900

    inferencer chech 방법

---
+++ 02_EDA/01_language_split.py
@@ -0,0 +1,41 @@
+import json
+import os
+import re
+
+
+def split_json_by_language(input_json_path):
+    # JSON 파일 읽기
+    with open(input_json_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # 언어별 데이터를 저장할 딕셔너리 초기화
+    language_data = {lang: {"images": {}} for lang in ["zh", "ja", "vi", "th"]}
+
+    pattern = re.compile(r'extractor\.(zh|ja|vi|th)\.')
+
+    language_map = {
+        "zh": "chinese",
+        "ja": "japanese",
+        "vi": "vietnamese",
+        "th": "thai"
+    }
+
+    # 각 이미지의 파일 이름을 확인하고 언어별로 분류
+    for image_name, image_data in data["images"].items():
+        match = pattern.search(image_name)
+        if match:
+            lang = match.group(1)
+            language_data[lang]["images"][image_name] = image_data
+        else:
+            print(f"Warning: Language code not found in {image_name}")
+
+    # 언어별 JSON 파일로 저장
+    for lang, lang_data in language_data.items():
+        output_path = f"./split/{language_map[lang]}_output.json"
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(lang_data, f, ensure_ascii=False, indent=4)
+        print(f"Saved {output_path}")
+
+
+# 예측 json 파일 경로
+split_json_by_language('1024_output.json')
