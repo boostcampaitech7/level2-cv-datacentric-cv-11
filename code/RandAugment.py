7570120deb13e152e0cbd8fe6008f53df88efe04
Daehwan <aiart3536@gmail.com>
2024-11-08 11:58:23 +0900

    [add] train, dataset, inference

---
+++ code/RandAugment.py
@@ -0,0 +1,115 @@
+# code in this file is adpated from rpmcruz/autoaugment
+# https://github.com/rpmcruz/autoaugment/blob/master/transformations.py
+import random
+
+import PIL
+import albumentations as A
+import numpy as np
+import torch
+from PIL import Image
+
+
+def augment_list():  # 16 oeprations and their ranges
+    # https://github.com/google-research/uda/blob/master/image/randaugment/policies.py#L57
+    # l = [
+    #     (Identity, 0., 1.0),
+    #     (ShearX, 0., 0.3),  # 0
+    #     (ShearY, 0., 0.3),  # 1
+    #     (TranslateX, 0., 0.33),  # 2
+    #     (TranslateY, 0., 0.33),  # 3
+    #     (Rotate, 0, 30),  # 4
+    #     (AutoContrast, 0, 1),  # 5
+    #     (Invert, 0, 1),  # 6
+    #     (Equalize, 0, 1),  # 7
+    #     (Solarize, 0, 110),  # 8
+    #     (Posterize, 4, 8),  # 9
+    #     # (Contrast, 0.1, 1.9),  # 10
+    #     (Color, 0.1, 1.9),  # 11
+    #     (Brightness, 0.1, 1.9),  # 12
+    #     (Sharpness, 0.1, 1.9),  # 13
+    #     # (Cutout, 0, 0.2),  # 14
+    #     # (SamplePairing(imgs), 0, 0.4),  # 15
+    # ]
+
+    style_transform = [
+        A.RGBShift(r_shift_limit=30, g_shift_limit=20, b_shift_limit=20, p=1.0),
+        A.CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), p=1.0),
+        A.Posterize(num_bits=4, p=1.0),
+        A.Solarize(threshold=128, p=1.0),
+        A.Equalize(p=1.0),
+        A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=1.0),
+        A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=1.0),
+        A.FancyPCA(alpha=0.1, p=1.0),
+        A.GaussianBlur(blur_limit=(3, 7), p=1.0),
+        A.GaussNoise(mean=1, p=1.0),
+        A.RandomBrightnessContrast(brightness_limit=0, contrast_limit=(1 - 1.0, 1 + 1.0), p=1.0),
+        A.RandomBrightnessContrast(brightness_limit=(1 - 1.0, 1 + 1.0), contrast_limit=0, p=1.0),
+        A.Posterize(num_bits=5, p=1.0),
+        A.ColorJitter(p=1.0)
+    ]
+
+    return style_transform
+
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = torch.Tensor(eigval)
+        self.eigvec = torch.Tensor(eigvec)
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone() \
+            .mul(alpha.view(1, 3).expand(3, 3)) \
+            .mul(self.eigval.view(1, 3).expand(3, 3)) \
+            .sum(1).squeeze()
+
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+class CutoutDefault(object):
+    """
+    Reference : https://github.com/quark0/darts/blob/master/cnn/utils.py
+    """
+
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
+
+
+class RandAugment:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m      # [0, 30]
+        self.augment_list = augment_list()
+
+    def __call__(self, img):
+        ops = random.choices(self.augment_list, k=self.n)
+        transform = A.Compose(ops)
+        img = np.array(img)
+
+        transformed = transform(image=img)
+        img = transformed['image']
+
+        return img
---
+++ code/inference_val.py
@@ -0,0 +1,105 @@
+import os
+import os.path as osp
+import json
+from argparse import ArgumentParser
+from glob import glob
+
+import torch
+import cv2
+from torch import cuda
+from model import EAST
+from tqdm import tqdm
+import numpy as np
+
+from detect import detect
+
+CHECKPOINT_EXTENSIONS = ['.pth', '.ckpt']
+LANGUAGE_LIST = ['chinese', 'japanese', 'thai', 'vietnamese', 'custom']
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+    # Conventional args
+    parser.add_argument('--data_dir', default=os.environ.get('SM_CHANNEL_EVAL', 'data'))
+    parser.add_argument('--model_dir', default=os.environ.get('SM_CHANNEL_MODEL', 'trained_models'))
+    parser.add_argument('--output_dir', default=os.environ.get('SM_OUTPUT_DATA_DIR', 'predictions'))
+
+    parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
+    parser.add_argument('--input_size', type=int, default=2048)
+    parser.add_argument('--batch_size', type=int, default=8)
+
+    args = parser.parse_args()
+
+    if args.input_size % 32 != 0:
+        raise ValueError('`input_size` must be a multiple of 32')
+
+    return args
+
+
+def sharpening(image, strength):
+    image = image.astype('uint8')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    b = (1 - strength) / 8
+    sharpening_kernel = np.array([[b, b, b],
+                                  [b, strength, b],
+                                  [b, b, b]])
+    kernel = np.ones((3, 3), np.uint8)
+    gray_image = cv2.erode(gray_image, kernel, iterations=1)
+    output = cv2.filter2D(gray_image, -1, sharpening_kernel)
+    output = cv2.cvtColor(output, cv2.COLOR_GRAY2RGB)
+    return output
+
+
+def do_inference(model, ckpt_fpath, data_dir, input_size, batch_size, split='train'):
+    model.load_state_dict(torch.load(ckpt_fpath, map_location='cpu'))
+    model.eval()
+
+    image_fnames, by_sample_bboxes = [], []
+
+    images = []
+
+    for image_fpath in tqdm(sum([glob(osp.join(data_dir, f'{lang}_receipt/img/{split}/*')) for lang in LANGUAGE_LIST], [])):
+        image_fnames.append(osp.basename(image_fpath))
+
+        images.append(sharpening(cv2.imread(image_fpath), 7)[:, :, ::-1])
+        if len(images) == batch_size:
+            by_sample_bboxes.extend(detect(model, images, input_size))
+            images = []
+
+    if len(images):
+        by_sample_bboxes.extend(detect(model, images, input_size))
+
+    ufo_result = dict(images=dict())
+    for image_fname, bboxes in zip(image_fnames, by_sample_bboxes):
+        words_info = {idx: dict(points=bbox.tolist()) for idx, bbox in enumerate(bboxes)}
+        ufo_result['images'][image_fname] = dict(words=words_info)
+
+    return ufo_result
+
+
+def main(args):
+    # Initialize model
+    model = EAST(pretrained=False).to(args.device)
+
+    # Get paths to checkpoint files
+    ckpt_fpath = osp.join(args.model_dir, 'epoch_150.pth')
+
+    if not osp.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    print('Inference in progress')
+
+    ufo_result = dict(images=dict())
+    split_result = do_inference(model, ckpt_fpath, args.data_dir, args.input_size,
+                                args.batch_size, split='train')  # Change split to 'val'
+    ufo_result['images'].update(split_result['images'])
+
+    output_fname = 'val_data.json'  # Change to .json
+    with open(osp.join(args.output_dir, output_fname), 'w') as f:
+        json.dump(ufo_result, f, indent=4)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
